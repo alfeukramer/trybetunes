ebaec9fe4e2816dea2d235b7f84a44d4a0360c17
Alfeu Kramer <alfeukramer@gmail.com>
2022-05-23 19:15:20 -0300

    Requisito 01

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        <h2>login screen</h2>
+      </div>
+    );
+  }
+}
+
+export default Login;
