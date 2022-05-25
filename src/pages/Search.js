317cfd2b465a7c1cbb4957eaf96bf4403698b211
Alfeu Kramer <alfeukramer@gmail.com>
2022-05-24 22:08:14 -0300

    Requisito 05

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,36 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.onChangeFunction = this.onChangeFunction.bind(this);
+    this.state = { hasLength: true };
+  }
+
+  onChangeFunction({ target: { value } }) {
+    const LENGTH_NUMBER = 2;
+    this.setState({ name: value, hasLength: value.length < LENGTH_NUMBER });
+  }
+
   render() {
+    const { hasLength, name } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h2>Search</h2>
+        <input
+          data-testid="search-artist-input"
+          placeholder="Digite a banda ou artista a ser pesquisada..."
+          value={ name }
+          onChange={ this.onChangeFunction }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ hasLength }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
